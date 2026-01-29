03ea7d8df22d7fee284b37e84336a82382b161bf
Thomas McCarthy <thomasjmmccarthy@gmail.com>
2026-01-29 16:44:44 +0000

    Added GithubPages setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const redirect = sessionStorage.redirect;
+if (redirect) {
+  sessionStorage.removeItem('redirect');
+  window.history.replaceState(null, null, redirect);
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
